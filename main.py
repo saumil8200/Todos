769464a1715a2002448d8a5b376f5180ccadd545
Saumil Shah <saumil8200@gmail.com>
2025-03-21 15:08:30 +0530

    POST req add todo validations

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import Annotated
+
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
+
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -17,6 +21,28 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found.")
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db:db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
